156d6dc40addd40548d03e7680043c084a288458
saqib <saqib.idrees95@gmail.com>
2022-11-22 17:05:46 +0500

    package initial setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+export default function OutsideClickHandler({children, onClickOutside, ...props}) {
+  const wrapperRef = useRef(null);
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        onClickOutside();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef]);
+  return (
+    <div ref={wrapperRef} {...props}>
+      {children}
+    </div>
+  );
+  }
